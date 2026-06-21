d1502d9744b8263ee54cfb53e5d450505519d0bf
linkbrus-jpg <linkbrus@gmail.com>
2026-06-21 17:49:06 +0700

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("androidx.media3:media3-exoplayer:1.2.1")
+    implementation("androidx.media3:media3-ui:1.2.1")
+ 
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
